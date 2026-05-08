ecd4196f8fcd0e5faf6af00983f17d6a3cda7bbf
Pakjira <268345221014-st@rmutsb.ac.th>
2026-05-08 15:03:28 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+console.log("Pet Shop Website");
+function openModal() {
+  document.getElementById("modal").classList.remove("hidden");
+}
+
+function closeModal() {
+  document.getElementById("modal").classList.add("hidden");
+}
